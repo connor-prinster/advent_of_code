0bcc492984fa6e7ee0856ecccbffa3c560156e82
Connor Prinster <connor.prinster@gmail.com>
2022-12-01 17:00:20 -0700

    day one

---
+++ advent_of_code_2022/src/main/kotlin/dayone/DayOne.kt
@@ -0,0 +1,22 @@
+package dayone
+
+import java.io.File
+
+class DayOne {
+    fun retrieveDataFromFile(): List<Int> {
+        return File("src/main/kotlin/dayone/day_one.txt")
+            .useLines { it ->
+                it.toList().joinToString().split(", ,").map {elfList ->
+                    var calories = 0
+                    elfList.splitToSequence(",").forEach{ item ->
+                        calories += item.filter { !it.isWhitespace() }.toInt()
+                    }
+                    calories
+                }
+            }
+    }
+}
+
+fun main(args: Array<String>) {
+    print("Max calories being carried: ${DayOne().retrieveDataFromFile().maxOrNull()}")
+}
