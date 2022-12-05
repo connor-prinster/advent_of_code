43ce7aaaaa1d548ee5a69fedaafb0646fba48880
Connor Prinster <connorprinster@churchofjesuschrist.org>
2022-12-05 09:26:55 -0700

    better

--- advent_of_code_2022/src/main/kotlin/dayfour/DayFour.kt
+++ advent_of_code_2022/src/main/kotlin/dayfour/DayFour.kt
@@ -3,24 +3,90 @@ package dayfour
 import daythree.RuckSack
 import java.io.File
 
-data class AssignmentPairs(val pairOne: String, val pairTwo: String)
+data class AssignmentPairs(val pairOne: FirstLast, val pairTwo: FirstLast)
+data class FirstLast(val start: Int, val finish: Int) {
+    fun size(): Int = finish - start
+}
 
 class DayFourPartOne {
     private fun getAssignmentPairs(filename: String): List<AssignmentPairs> {
         return File(filename)
-            .useLines { it ->
-                it.toList().map {
-                    AssignmentPairs("", "")
+            .useLines {
+                it.toList().map {pairs ->
+                    val assignmentPairs = pairs.split(",")
+                    val firstLasts = assignmentPairs.map { firstLastString ->
+                        val firstLastInts = firstLastString.split("-")
+                        FirstLast(firstLastInts[0].toInt(), firstLastInts[1].toInt())
+                    }
+
+                    AssignmentPairs(firstLasts[0], firstLasts[1])
                 }
             }
     }
 
-    fun countPairsContainingTheOther(filename: String = "src/main/kotlin/dayfour/input.txt"): Int {
-        getAssignmentPairs(filename)
-        return 0
+    private fun countFullyContainedPairs(pairs: List<AssignmentPairs>): Int {
+        var count = 0
+
+        pairs.forEach {pair ->
+            count += if (
+                doesPairContainTheOther(
+                    pairOne = pair.pairOne,
+                    pairTwo = pair.pairTwo
+                ) || doesPairContainTheOther(
+                    pairOne = pair.pairTwo,
+                    pairTwo = pair.pairOne
+                )) 1 else 0
+        }
+
+        return count
+    }
+
+    private fun countOverlappingPairs(pairs: List<AssignmentPairs>): Int {
+        var count = 0
+
+
+        pairs.forEach {pair ->
+            val arrayOne = makeArrayFromPair(pair.pairOne)
+            val arrayTwo = makeArrayFromPair(pair.pairTwo)
+
+            count += if (
+                doesPairOverlapTheOther(
+                    arrayOne, arrayTwo
+                ) || doesPairOverlapTheOther(
+                    arrayTwo, arrayOne
+                )) 1 else 0
+        }
+
+        return count
+    }
+
+    private fun makeArrayFromPair(firstLast: FirstLast): Array<Int> {
+        val array = mutableListOf<Int>()
+        for (i in firstLast.start.. firstLast.finish) {
+            array.add(i)
+        }
+
+        return array.toTypedArray()
+    }
+
+    private fun doesPairContainTheOther(pairOne: FirstLast, pairTwo: FirstLast): Boolean {
+        return pairOne.start <= pairTwo.start &&
+                        pairOne.finish >= pairTwo.finish
+    }
+
+    private fun doesPairOverlapTheOther(pairOne: Array<Int>, pairTwo: Array<Int>): Boolean {
+        return pairOne.intersect(pairTwo.toList().toSet()).isNotEmpty()
+    }
+
+    fun countPairsContainingTheOther(filename: String): Int {
+        return countFullyContainedPairs(getAssignmentPairs(filename))
+    }
+    fun countPairsOverlappingTheOther(filename: String): Int {
+        return countOverlappingPairs(getAssignmentPairs(filename))
     }
 }
 
 fun main(args: Array<String>) {
-    println("Assignment pairs containing the other: ${DayFourPartOne().countPairsContainingTheOther("src/main/kotlin/dayfour/test_input.txt")}")
+    println("Assignment pairs containing the other: ${DayFourPartOne().countPairsContainingTheOther("advent_of_code_2022/src/main/kotlin/dayfour/input.txt")}")
+    println("Assignment pairs overlapping at all: ${DayFourPartOne().countPairsOverlappingTheOther("advent_of_code_2022/src/main/kotlin/dayfour/input.txt")}")
 }
