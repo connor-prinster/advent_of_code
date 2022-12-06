0e73cecd9a018c637f491cbd6aaef4cae01de0b7
Connor Prinster <connorprinster@churchofjesuschrist.org>
2022-12-06 10:41:16 -0700

    day six done

--- advent_of_code_2022/src/main/kotlin/daysix/DaySix.kt
+++ advent_of_code_2022/src/main/kotlin/daysix/DaySix.kt
@@ -1,6 +1,7 @@
 package daysix
 
 import java.io.File
+import java.util.Collections
 
 data class Packet(val packet: String)
 
@@ -17,13 +18,45 @@ class DaySix {
         return packets
     }
 
-    fun howManyCharacters(filename: String): String {
-        val packets = parseFile(filename)
-        return packets.toString()
+    private fun findFirstDistinctCharacters(packet: String, numberOfCharacters: Int): Int {
+        val characters: ArrayList<Char> = arrayListOf()
+        packet.toList().forEachIndexed { index, char ->
+            if (characters.size < numberOfCharacters) {
+                characters.add(char)
+            }
+            else {
+                if (listContainsAnythingTwice(characters)) {
+                    characters.removeFirst()
+                    characters.add(char)
+                } else {
+                    return index
+                }
+            }
+        }
+
+        return -1
+    }
+
+    private fun listContainsAnythingTwice(list: ArrayList<Char>): Boolean {
+        for (item in list.distinct()) {
+            if (Collections.frequency(list, item) > 1) return true
+        }
+        return false
+    }
+
+    fun howManyCharactersBeforeMarker(filename: String, numberOfCharacters: Int): String {
+        val positions: ArrayList<Int> = arrayListOf()
+        parseFile(filename).forEach {
+            positions.add(findFirstDistinctCharacters(packet = it.packet, numberOfCharacters = numberOfCharacters))
+        }
+        return positions.toString()
     }
 }
 
 fun main(args: Array<String>) {
-    println("How many characters must be parsed: ${DaySix().howManyCharacters("advent_of_code_2022/src/main/kotlin/daysix/test_input.txt")}")
-//    println("Top of each stack Crane9001: ${DayFive().findTopCrate("advent_of_code_2022/src/main/kotlin/dayfive/input.txt", is9001 = true)}")
+    val filename = "advent_of_code_2022/src/main/kotlin/daysix/input.txt"
+//    [7, 5, 6, 10, 11]
+    println("How many characters must be parsed for marker: ${DaySix().howManyCharactersBeforeMarker(filename, 4)}")
+//    [19, 23, 23, 29, 26]
+    println("How many characters must be parsed for message: ${DaySix().howManyCharactersBeforeMarker(filename, 14)}")
 }
